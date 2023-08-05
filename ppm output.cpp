4294f830945f5ee1c45694c0cc5642859c8e4e92
Frederick <antvenomxd72@gmail.com>
2023-08-05 17:02:40 -0300

    add ppm estructure code

---
+++ ppm output.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    int cols = 200;
+    int lines = 100;
+
+    // estrutura de um arquivo ppm
+    std::cout << "P3\n";
+    std::cout << cols << " " << lines << "\n255\n";
+
+    int i, j, ir, ig, ib;
+    float r, g, b;
+
+    for (j = lines-1; j >= 0; j--) {
+        for (i = 0; i < cols; i++) {
+            // r, g, b variam de 0 ate 1, por convencao
+            r = float(i) / float(cols);
+            g = float(j)/float(lines);
+            b = 0.2;
+
+            // multiplicar para ficarem no range de 0 a 255
+            ir = int(255.99*r);
+            ig = int(255.99*g);
+            ib = int(255.99*b);
+
+            // printar os pixels
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+}
