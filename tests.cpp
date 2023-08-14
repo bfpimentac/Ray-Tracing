f4f7e3f9cda5fae7d13ac388fa1ad9c773afeb5a
Frederick <antvenomxd72@gmail.com>
2023-08-14 18:03:19 -0300

    feat: sphere intersection with simple shading

--- tests.cpp
+++ tests.cpp
@@ -46,16 +46,18 @@ int main() {
     //std::cout << vetor3 << '\n';
 
     // retorna a norma
-    std::cout << vetor1.length() << "\n";
+    vec3 vetor3(3, 4, 0);
+    std::cout << "length: " << vetor3.length() << "\n";
+    std::cout << "unit vector: " << unit_vector(vetor3) << "\n";
 
     // retorna a norma ao quadrado
-    std::cout << vetor1.squared_length() << "\n";
+    std::cout << vetor3.squared_length() << "\n";
 
     // acessando o index i do vetor
-    std::cout << vetor1[0] << "\n";
+    std::cout << vetor3[0] << "\n";
 
     // retornando o vetor com coordenadas invertidas
-    std::cout << -vetor1 << "\n";
+    std::cout << -vetor3 << "\n";
 
     // retorna a coordenada x, y e z do vetor
     std::cout << vetor1.x() << " " << vetor1.y() << " " << vetor1.z() << "\n";
@@ -68,4 +70,6 @@ int main() {
     std::cout << 0.7*vec3(1, 1, 1) + 0.3*vec3(0.5, 0.7, 1.0)<< "\n";
 
     std::cout << r1.origin() - vec3(0, 0, -1) << "\n";
+
+    std::cout << unit_vector(r1.point_at_parameter(2) - vec3(0, 0, -1)) << "\n";
 }
