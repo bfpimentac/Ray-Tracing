19a3ee0ab955a0ab3fffd9c68e8cd376679d9e95
Frederick <antvenomxd72@gmail.com>
2023-08-14 04:35:50 -0300

    create ray class

---
+++ ray.h
@@ -0,0 +1,31 @@
+#ifndef RAYH
+#define RAYH
+#include "point and vector.h"
+
+
+class ray {
+    public:
+        ray() {}
+        ray(const vec3& p, const vec3& v) {
+            point = p;
+            vector = v;
+        }
+
+        vec3 origin() const {
+            return point;
+        }
+
+        vec3 direction() const {
+            return vector;
+        }
+
+        vec3 point_at_parameter(float t) const {
+            return point + t*vector;
+        }
+
+        vec3 point;
+        vec3 vector;
+};
+
+
+#endif
