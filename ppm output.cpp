54271d672504bf0527769c3d4fe798f778449dfb
Frederick <antvenomxd72@gmail.com>
2023-08-22 22:51:43 -0300

    feat(diffuse material): recursive shadows

--- ppm output.cpp
+++ ppm output.cpp
@@ -7,12 +7,28 @@
 
 #include "float.h"
 
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<float> dist(0.0, 1.0);
+
+// pega um ponto aleatorio da esfera unitaria
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(dist(gen), dist(gen), dist(gen)) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+
+    return p;
+}
+
 
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
     if (world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
 
+        return 0.5*color(ray(rec.p, target-rec.p), world); // recursao
+        //return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
     } else {
         // normaliza o vetor diretor do raio
         vec3 unit_direction = unit_vector(r.direction());
@@ -22,14 +38,9 @@ vec3 color(const ray& r, hitable *world) {
 }
 
 int main() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    std::uniform_real_distribution<float> dist(0.0, 1.0);
-
     int nx = 600;
     int ny = 300;
-    int ns = 300; // numero de amostras
+    int ns = 300; // numero de amostras p anti-aliasing
 
     // estrutura de um arquivo ppm
     std::cout << "P3\n";
@@ -38,6 +49,7 @@ int main() {
     hitable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    //list[2] = new sphere(vec3(0, 0, -120), 100);
     hitable *world = new hitable_list(list, 2);
 
     int i, j, s, ir, ig, ib;
